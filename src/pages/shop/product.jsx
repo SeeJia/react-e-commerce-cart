aaca76d1da07365120c8c90e4c13151017cb7d00
SeeJia <seejiajia1010@gmail.com>
2023-07-02 22:09:36 +0800

    add product detail function

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,12 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import {ShopContext} from '../../context/shop-context';
 
 export const Product = (props) => {
-    const { id,productName,price,productImage } = props.data;
+    const { id,productName,price,productImage, productDescription } = props.data;
     
     const { addToCart, cartItems } = useContext(ShopContext);
     
     const cartItemAmount = cartItems[id];
+
+    const [isTextVisible, setIsTextVisible] = useState(false);
+
+    const handleClick = () => {
+      setIsTextVisible(!isTextVisible);
+    };
+
+    const handleCancel = () => {
+      setIsTextVisible(false);
+    };
 
     return (
     <div className='product'>
@@ -16,6 +26,19 @@ export const Product = (props) => {
         <p>${price}</p>
       </div>
       <button className='addToCartBttn' onClick={() => addToCart(id)}>Add To Cart{cartItemAmount > 0 && <>({cartItemAmount})</>}</button>
+      <button className='addToCartBttn' onClick={handleClick}>Details</button>
+      {isTextVisible && 
+      
+      (<div className="fullscreen-text">
+        <img src={productImage} alt="product_image" />
+        <div className='description'>
+        <p><b>{productName}</b></p>
+        <p className='product-description'>{productDescription}</p>
+        <p>${price}</p>
+      </div>
+        <button className='BttnDark' onClick={() => addToCart(id)}>Add To Cart{cartItemAmount > 0 && <>({cartItemAmount})</>}</button>
+        <button className='BttnDark' onClick={handleCancel}>Cancel</button>  
+      </div>)}
     </div>
   )
 }
--- src/products.js
+++ src/products.js
@@ -12,48 +12,56 @@ export const Products =[
         id:1,
         productName: "Product1",
         price:999.00,
-        productImage:products1
+        productImage:products1,
+        productDescription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, inventore. Molestiae eveniet repellendus voluptate possimus obcaecati deleniti consequatur numquam maiores magnam eaque, sapiente deserunt, sunt quam similique quaerat illo iste.",
     },
     {
         id:2,
         productName: "Product2",
         price:3999.00,
-        productImage:products2
+        productImage:products2,
+        productDescription: "This is product 2.",
     },
     {
         id:3,
         productName: "Product3",
         price:4999.00,
-        productImage:products3
+        productImage:products3,
+        productDescription: "This is product 3.",
     },
     {
         id:4,
         productName: "Product4",
         price:99.00,
-        productImage:products4
+        productImage:products4,
+        productDescription: "This is product 4.",
     },
     {
         id:5,
         productName: "Product5",
         price:5999.00,
-        productImage:products5
+        productImage:products5,
+        productDescription: "This is product 5.",
     },
     {
         id:6,
         productName: "Product6",
         price:59.00,
-        productImage:products6
+        productImage:products6,
+        productDescription: "This is product 6.",
     },
     {
         id:7,
         productName: "Product7",
         price:159.00,
-        productImage:products7
+        productImage:products7,
+        productDescription: "This is product 7.",
     },
     {
         id:8,
         productName: "Product8",
         price:199.00,
-        productImage:products8
+        productImage:products8,
+        productDescription: "This is product 8.",
     },
 ];
